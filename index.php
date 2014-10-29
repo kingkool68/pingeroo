3461c56206441ee24e7dcfb61a40686fe3c09431
Russell Heimlich <kingkool68@gmail.com>
2014-10-28 22:24:45 -0400

    Added a Settings link at the bottom.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ wp_enqueue_script('pingeroo');
 
 show_admin_bar( false );
 remove_action('wp_head', '_admin_bar_bump_cb');
+$options = get_pingeroo_options();
 
 get_header();
 ?>
@@ -30,18 +31,10 @@ get_header();
 		<div class="main">
 			<fieldset id="the-message">
 				<label for="message" class="hidden">Message</label>
-				<textarea id="message" name="message" rows="1"></textarea>
+				<textarea id="message" name="message" rows="1"><?php echo $options['pre-fill']; ?></textarea>
 			</fieldset>
 			
-			<div class="added-media">
-				<!--div class="media">
-					<p class="percent">0%</p>
-					<div class="progress">
-						<div class="slice first-half"></div>
-						<div class="slice second-half"></div>
-					</div>
-				</div-->
-			</div>
+			<div class="added-media"></div>
 			
 			<p class="buttons">
 				<button title="Media" class="media" id="media-upload"><i class="icon-pictures"></i></button>
@@ -69,6 +62,7 @@ get_header();
 		</div>
 		
 		<input type="hidden" name="pingeroo-nonce" value="<?php echo wp_create_nonce( 'do-pingeroo' );?>">
+		<input type="hidden" name="action" value="save_pingeroo_post">
 	</form>
 	
 	<section id="recent" style="display:none;">
@@ -81,7 +75,10 @@ get_header();
 	</section>
 </div>
 
+<p class="settings"><a href="<?php echo admin_url('options-general.php?page=pingeroo'); ?>"><i class="icon-cog" aria-hidden="true"></i>Settings</a></p>
+
 <div id="drop-target">
 	<h2><i class="icon-upload" aria-hidden="true"></i>Drop files here to upload</h2>
 </div>
+
 <?php get_footer(); ?>
